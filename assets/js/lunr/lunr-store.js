6af3b5312d5f41ceff683a4f26eeb3d539974220
sigwinch28 <sigwinch28@users.noreply.github.com>
2020-12-01 10:50:35 +0000

    jekyll build from Action 56162bb80cee8a4350d5ebaf239734741e2df1fe

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,133 @@
+var store = [{
+        "title": "🚧 Attending Court",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/attending-court/",
+        "teaser": null
+      },{
+        "title": "🚧 Battery Life",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/battery-life/",
+        "teaser": null
+      },{
+        "title": "🚧 Quick Start",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/camera-quick-start/",
+        "teaser": null
+      },{
+        "title": "🚧 Cameras as Crime Deterrents",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/cameras-as-crime-deterrents/",
+        "teaser": null
+      },{
+        "title": "🚧 Choosing a Camera",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/choosing-a-camera/",
+        "teaser": null
+      },{
+        "title": "🚧 Email Responses from the Met",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/email-responses/",
+        "teaser": null
+      },{
+        "title": "🚧 Frequently Asked Questions",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/faq/",
+        "teaser": null
+      },{
+        "title": "🚧 GPS & Timestamps",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/gps-and-timestamps/",
+        "teaser": null
+      },{
+        "title": "Welcome",
+        "excerpt":"Welcome to the London Bikecam Guide :wave: This site is intended to serve as a comprehensive guide for anyone who wants to use a video camera while cycling in Greater London. If you are considering using a camera while cycling, start by thinking about why you might want to run...","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/",
+        "teaser": null
+      },{
+        "title": "🚧 Making Video Clips",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/making-video-clips/",
+        "teaser": null
+      },{
+        "title": "🚧 Mounting",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/mounting/",
+        "teaser": null
+      },{
+        "title": "🚧 Offences & The Road Traffic Act",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/offences/",
+        "teaser": null
+      },{
+        "title": "🚧 Posting on Social Media",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/posting-on-social-media/",
+        "teaser": null
+      },{
+        "title": "🚧 Report A Collision",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/report-a-collision/",
+        "teaser": null
+      },{
+        "title": "Report a Driving Offence",
+        "excerpt":"If you’ve witnessed a driving offence or have been in involved in an traffic incident where no people or vehicles collided, you can submit a report online. You have a maximum of 10 days to report a driving offence. The Metropolitan Police have 14 days from the time of the...","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/report-a-driving-offence/",
+        "teaser": null
+      },{
+        "title": "🚧 Resolution, Framerate, & FOV",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/resolution-framerate-fov/",
+        "teaser": null
+      },{
+        "title": "🚧 Reviewing Your Footage",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/reviewing-your-footage/",
+        "teaser": null
+      },{
+        "title": "🚧 Starting and Stopping your Camera",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/starting-and-stopping/",
+        "teaser": null
+      },{
+        "title": "What to Do after an Incident",
+        "excerpt":"If you’re cycling and you have your camera running, there are a few steps you can take to make sure your footage is usable as police evidence and so that it is easy to review. These steps aren’t strictly necessary, but they’re the ones I follow. There are additional steps...","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/what-to-do-after-an-incident/",
+        "teaser": null
+      },{
+        "title": "🚧 What to Do after an RTC",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/what-to-do-after-an-rtc/",
+        "teaser": null
+      },{
+        "title": "🚧 Why Run a Camera?",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/why-run-a-camera/",
+        "teaser": null
+      },{
+        "title": "Your Rights on the Road",
+        "excerpt":"Lots of people will tell you lots of little lies about how cyclists should and should not behave on the roads on the UK. Many of these people may not have ever read the highway code. Some of them may be angry drivers shouting at you for exercising your legal...","categories": [],
+        "tags": [],
+        "url": "https://sigwinch28.github.io/bikecam/docs/your-rights-on-the-road/",
+        "teaser": null
+      },]
